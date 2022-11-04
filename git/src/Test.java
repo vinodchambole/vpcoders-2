c197afc535ae79c663a941affb371bc51b5c7db5
vcham <vinodchambole@gmail.com>
2022-11-04 06:39:48 +0530

    added login code to test.java

--- git/src/Test.java
+++ git/src/Test.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Test {
+public class Test {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
 }
